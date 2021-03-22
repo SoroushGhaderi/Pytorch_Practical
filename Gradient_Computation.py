b3cb397180efbe30e03f0c5a62bc33deda98e540
Soroush <ghaderi.soroush1995@gmail.com>
2021-03-22 10:45:48 +0430

    backward and ways of ignore gradient

--- Gradient_Computation.py
+++ Gradient_Computation.py
@@ -1,2 +1,24 @@
 import torch
 
+# create random tensor and set requires grad equals True
+x = torch.randn(3, requires_grad=True)
+print(x)
+
+# pytorch detect add operation
+y = x + 2
+print(y)
+
+# pytorch detect mul operation
+z = y * y * 2
+z = z.mean()
+print(z)
+
+# dz/dx
+z.backward()
+print(x.grad)
+
+# we can set requires grad equals false in 3 ways
+# x.requires_grad_(False)
+# x.detach
+# with torch.no_grad():
+
