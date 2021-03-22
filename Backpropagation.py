8f6803aa8693d92bb72af875da65445434884c7c
Soroush <ghaderi.soroush1995@gmail.com>
2021-03-22 11:28:26 +0430

    backpropagation computation

--- Backpropagation.py
+++ Backpropagation.py
@@ -1,2 +1,16 @@
 import torch
 
+x = torch.tensor(1.0)
+y = torch.tensor(2.0)
+
+w = torch.tensor(1.0 , requires_grad=True)
+
+# forward pass
+y_hat = w * x
+# compute loss function
+loss = (y_hat - y) ** 2
+print(loss)
+
+# backpropagation
+loss.backward()
+print(w.grad)
