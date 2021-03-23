5e7bd80b3b4b954ea39a9ad585c3655fe1641d02
Soroush <ghaderi.soroush1995@gmail.com>
2021-03-23 10:55:10 +0430

    change file names and start create gradients with numpy

---
+++ 4.Gradients_Numpy.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+# create arrays f = 3x
+X = np.array([1, 2, 3, 4])
+Y = np.array([3, 6, 9, 12])
+# weight initialization
+w = np.array([0.1, 0.2, 0.13, 0.4])
+
+def forward(x):
+    return x * w
+
+
+
+
+def main():
+
+
+
+
+
+if __name__ == '__main__':
+    main()
