3c33e3f158a2d64649b801b74ad515971af37582
Soroush <ghaderi.soroush1995@gmail.com>
2021-04-03 12:20:52 +0430

    complete mlp neural network class

--- 10.Feedforward_Network.py
+++ 10.Feedforward_Network.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
-import matplotlib.pyplot as plt
 
 
 # device configuration
@@ -46,3 +45,41 @@ class NeuralNet(nn.Module):
 
 model = NeuralNet(input_size=input_dim, hidden_size=hidden_dim,
                   num_classes=n_classes)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# training loop
+n_total_step = len(train_dataloader)
+for epoch in range(n_epoch):
+    for i, (images, labels) in enumerate(train_dataloader):
+        # flat picture
+        images = images.reshape(-1, 28*28).to(device)
+        labels = labels.to(device)
+        # forward pass
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        # backward pass
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (i+1) % 100 == 0:
+            print (f'Epoch [{epoch+1}/{n_epoch}], Step [{i+1}/{n_total_step}], Loss: {loss.item():.4f}')
+
+
+# test and evaluation
+with torch.no_grad():
+    n_correct = 0
+    n_samples = 0
+    for images, labels in test_dataloader:
+        images = images.reshape(-1, 28*28).to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+        # max returns (value ,index)
+        _, predicted = torch.max(outputs.data, 1)
+        n_samples += labels.size(0)
+        n_correct += (predicted == labels).sum().item()
+
+    acc = 100.0 * n_correct / n_samples
+    print(f'Accuracy of the network on the 10000 test images: {acc} %')
